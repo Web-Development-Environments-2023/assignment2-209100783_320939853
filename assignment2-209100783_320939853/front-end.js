75610d88d1ab58c74cfd51ca7d93f85429c5f00d
TseytlinMark <mtseytlin345@gmail.com>
2023-04-11 18:45:45 +0300

    Configuration Page, Shooting Key Configuration

--- assignment2-209100783_320939853/front-end.js
+++ assignment2-209100783_320939853/front-end.js
@@ -6,6 +6,12 @@ var passwordInput;
 var loginPagebtn;
 var registerPagebtn;
 var loginbtn;
+//
+var CurrUserName;
+//
+var configbtn;
+//
+var ShootingKeyCode;
 function initbtns(){
    loginPagebtn = document.getElementById("moveToLoginPage");
    loginPagebtn.addEventListener("click",loginPage,false);
@@ -22,6 +28,11 @@ function initbtns(){
 
    loginbtn = document.getElementById("loginbtn");
    loginbtn.addEventListener("click",verifyCredentials,false);
+
+   configbtn = document.getElementById("SaveConfigBtn");
+   configbtn.addEventListener("click",Configuration, false);
+
+   
    
 
 
@@ -36,9 +47,6 @@ function loginPage(){
    pastPage.style.display = "none";
    page.style.display = "grid";
 }
-
-
-
 function clearPasswordbtnText(){
    passwordInput.value = "";
 }
@@ -53,12 +61,13 @@ function registerPage(){
 
 function verifyCredentials(){
    let usename = String(usernameInput.value).replace(" ","");
+   CurrUserName = usename;
    let password = String(passwordInput.value).replace(" ","");
  
    if(usersTablePasswords.has(usename)){
       let tmppass = usersTablePasswords.get(usename);
       if (password == tmppass) {
-         validUser(usename);
+         validUser();
          
       }
       else{
@@ -71,19 +80,61 @@ function verifyCredentials(){
       notValidUser();
    }
 }
-function validUser(username){
-   dispatchEvent(new CustomEvent("verifeduser",{detail:
-      {
-      "usertable":userToRegisteredPlayer,
-      "username":username
-      }
-   }));
+function validUser(){
+   //Here -> Move To Configuration Screen
+   ConfigurationPage();
    console.log("user has been valideate and game has been init");
-   document.getElementById("gamePage").style.display = "grid";
-   document.getElementById('')
-   document.getElementById("loginPage").style.display = "none"
-
 }
+function ConfigurationPage()
+{
+   //Remove Login Page
+   document.getElementById("configurationPage").style.display = "grid";
+   document.getElementById("loginPage").style.display = "none"
+   //Init Configuration Page
+   //At this meantime - there's event listener waiting for ConfigurationValidation. Configbtn ^above^
+}
+function ConfigurationValidation()
+{
+   let ShootingKey = document.getElementById("ShootingKeyInput").value;
+   if(ShootingKey.length == 1)
+   {
+      ShootingKeyCode = getKeyCode(ShootingKey);
+      console.log("ShootingKey : " + ShootingKey +" ShootingKeyCode : " + ShootingKeyCode);
+      if(ShootingKeyCode >= 48 && ShootingKeyCode <= 90 || ShootingKeyCode == 32)
+      {
+         return true;
+      }
+   }
+   return false;
+}
+function Configuration()
+{
+   //if all Configuration Parameters are Right ----> 
+   if(ConfigurationValidation())
+   {
+      //Go To Game
+      dispatchEvent(new CustomEvent("verifeduser",{detail:
+         {
+         "usertable":userToRegisteredPlayer,
+         "username":CurrUserName
+         }
+      }));
+      document.getElementById("configurationPage").style.display = "none";
+      document.getElementById("gamePage").style.display = "grid";
+   }
+   else
+   {
+      //Try Again 
+      window.alert("The Shooting Key You Chose is illegal.\nShooting Key Must Be One Key Press, Length Cant Be longer than 1.\n Shooting Key Must be a Letter (a-z) or SpaceBar.\n Please Try Again.");
+   }
+}
+function getKeyCode(char) {
+   let keyCode = char.charCodeAt(0);
+   if(keyCode > 90) {  // 90 is keyCode for 'z'
+     return keyCode - 32;
+   }
+   return keyCode;
+ }
 function notValidUser(){
 }
 
